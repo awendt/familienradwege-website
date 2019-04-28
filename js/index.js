6264c4489c24292efa6bf65f54212fc18f60b9bc
André Wendt <awendt@freeshell.org>
2019-04-28 21:16:06 +0200

    Let users interactively toggle layers

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ import { roads_layer, paths_layer } from './map_data';
 import Map from 'ol/Map';
 import View from 'ol/View';
 import { fromLonLat } from 'ol/proj';
+import LayerSwitcher from 'ol-layerswitcher';
 
 const BERLIN_COORDINATES = [13.410, 52.524];
 
@@ -15,3 +16,6 @@ var map = new Map({
     zoom: 13
   })
 });
+
+var layerSwitcher = new LayerSwitcher();
+map.addControl(layerSwitcher);
--- js/map_data.js
+++ js/map_data.js
@@ -2,6 +2,7 @@ import VectorLayer from 'ol/layer/Vector';
 import VectorSource from 'ol/source/Vector';
 import GeoJSON from 'ol/format/GeoJSON';
 import { Stroke, Style } from 'ol/style';
+import { Group as LayerGroup } from 'ol/layer';
 
 const default_style = new Style({
   stroke: new Stroke({
@@ -10,19 +11,22 @@ const default_style = new Style({
   })
 });
 
-// from https://openlayers.org/en/latest/examples/vector-layer.html?q=geojson
-export const roads_layer = new VectorLayer({
-  source: new VectorSource({
-    url: '/data/berlin/roads.json',
-    format: new GeoJSON()
-  }),
-  style: default_style
-});
+const build_layer = (title, url) => {
+  return new LayerGroup({
+    title: title,
+    combine: true,
+    layers: [
+      // https://openlayers.org/en/latest/examples/vector-layer.html?q=geojson
+      new VectorLayer({
+        source: new VectorSource({
+          url: url,
+          format: new GeoJSON()
+        }),
+        style: default_style
+      })
+    ]
+  });
+};
 
-export const paths_layer = new VectorLayer({
-  source: new VectorSource({
-    url: '/data/berlin/paths.json',
-    format: new GeoJSON()
-  }),
-  style: default_style
-});
+export const paths_layer = build_layer('Straßen', '/data/berlin/roads.json');
+export const roads_layer = build_layer('Wege', '/data/berlin/paths.json');
