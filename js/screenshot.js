0254d1df785e3763bcc031b7ee7802e9290a7dcb
André Wendt <awendt@freeshell.org>
2019-05-02 21:21:28 +0200

    Add a page to create screenshots
    Screenshots will have a different basemap
    (github.com/CartoDB/basemap-styles) which has usage limitations.
    
        npx parcel build screenshot.html --out-dir website

--- js/screenshot.js
+++ js/screenshot.js
@@ -4,13 +4,14 @@ import Map from 'ol/Map';
 import View from 'ol/View';
 import { fromLonLat } from 'ol/proj';
 import LayerSwitcher from 'ol-layerswitcher';
+import FileSaver from 'file-saver';
 
 const BERLIN_COORDINATES = [13.410, 52.524];
 
 var map = new Map({
   target: 'mapid',
   layers: [
-    create_tile_layer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png'),
+    create_tile_layer('https://{a-d}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png'),
     roads_layer,
     paths_layer
   ],
@@ -22,3 +23,17 @@ var map = new Map({
 
 var layerSwitcher = new LayerSwitcher();
 map.addControl(layerSwitcher);
+
+document.getElementById('export-png').addEventListener('click', function() {
+  map.once('rendercomplete', function(event) {
+    var canvas = event.context.canvas;
+    if (navigator.msSaveBlob) {
+      navigator.msSaveBlob(canvas.msToBlob(), 'map.png');
+    } else {
+      canvas.toBlob(function(blob) {
+        FileSaver.saveAs(blob, 'map.png');
+      });
+    }
+  });
+  map.renderSync();
+});
--- js/tiles.js
+++ js/tiles.js
@@ -1,8 +1,10 @@
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
 
-export const tile_layer = new TileLayer({
-  source: new OSM({
-    url: 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png'
-  })
-});
+export const create_tile_layer = (url) => {
+  return new TileLayer({
+    source: new OSM({
+      url: url
+    })
+  });
+};
